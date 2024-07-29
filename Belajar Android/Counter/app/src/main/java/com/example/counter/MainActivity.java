918da79d768e67a8e96157b719c016bc66501d28
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-07-29 13:48:53 +0700

    new

--- Belajar Android/Counter/app/src/main/java/com/example/counter/MainActivity.java
+++ Belajar Android/Counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        load();
+    }
+
+    public void load(){
+        tvHasil = findViewById(R.id.tvHasil);
+        
+    }
+
+    public void btnUp(View view) {
+        count++;
+        tvHasil.setText(const+"");
+    }
+
+    public void btnDown(View view) {
+        count--;
+       tvHasil.setText(count+"");
     }
 }
