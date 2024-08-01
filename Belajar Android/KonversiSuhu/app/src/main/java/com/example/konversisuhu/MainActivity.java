142f6073e5b79c94bffa6c9c6d02206e9db5a8e8
ainulyaqin04 <ainulganteng223@gmail.com>
2024-08-01 10:00:28 +0700

    baru
    new nwe

--- Belajar Android/KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
+++ Belajar Android/KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.konversisuhu;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    EditText etNilai;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        cToR();
     }
+
+    public void load(){
+        spinner = findViewById(R.id.spinner);
+        etNilai = findViewById(R.id.etNilai);
+        tvHasil = findViewById(R.id.tvHasil);
+    }
+
+    /*
+    public void isiSpinner(){
+        String[] isi = ("Celcius To Reamur", "Celcius To Fahrenheit", "Celcius To Kelvin");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+        spinner.setAdapter(adapter);
+    }
+    */
+
+
+    public void btnKonversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+
+        if(etNilai.getText().toString().equals("")){
+            Toast.makeText(this, "Nilai Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+        }else{
+            if(pilihan.equals("Celcius To Reamur")){
+                cToR();
+            }
+
+            if(pilihan.equals("Celcius To Fahrenheit")){
+                cToF();
+            }
+        }
+    }
+
+    public void cToR (){
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (4.0/5.0) * suhu;
+
+        tvHasil.setText(hasil +"");
+
+    }
+
+    public void cToF(){
+        Toast.makeText(this, "Belum Dibuat", Toast.LENGTH_SHORT).show();
+    }
+
 }
