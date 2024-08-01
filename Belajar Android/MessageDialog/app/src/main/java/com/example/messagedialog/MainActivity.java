a30373257464a88ce91fcb059bdb0abe8899220a
ainulyaqin04 <ainulganteng223@gmail.com>
2024-08-01 11:07:30 +0700

    ep1

---
+++ Belajar Android/MessageDialog/app/src/main/java/com/example/messagedialog/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.messagedialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        System.out.println("onStart");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        System.out.println("omResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        System.out.println("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        System.out.println("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        System.out.println("onDestroy   ");
+    }
+
+    
+}
